e2fe488509bb4f6bb2d807278092bee1315bd4fd
k-mitani <k-mitani@unique21.co.jp>
2023-02-05 20:54:03 +0900

    ui セーブデータ一覧コントロール ハリボテ作成

---
+++ Assets/UI/SaveDataListItem.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveDataListItem : MonoBehaviour
+{
+    [SerializeField] private Image mapImage;
+    [SerializeField] private TextMeshProUGUI townName;
+    [SerializeField] private TextMeshProUGUI saveType;
+    [SerializeField] private TextMeshProUGUI savedTime;
+    [SerializeField] private Button selectButton;
+    [SerializeField] private Button deleteButon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
